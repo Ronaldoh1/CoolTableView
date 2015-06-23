c65fb2f0b82edb7c35f2db00877dd35de8a5c1a8
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-06-23 15:18:07 -0400

    able to delete cell, able to add new item and display on table view

--- PrettyIcons2/ViewController.m
+++ PrettyIcons2/ViewController.m
@@ -25,9 +25,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
+    //to add the edit button at the top simply add the one that's already bilt in.
+
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
     //we neeed to get the sets to display in table view
 
-    self.iconSets = [IconSet iconSets];
+    self.iconSets = [IconSet iconSets ];
 
 }
 
@@ -48,14 +52,47 @@
     return set.name;
 }
 
+//in order to be able to delete an item from the table view you need to implement the following method and checkfor the diting style.
+//also this method is used to insert new rows to table.
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+
+        //delete the item from the data model.
+        IconSet *set = self.iconSets[indexPath.section];
+        [set.icons removeObjectAtIndex:indexPath.row];
+
+        //we need to remove it from the tableview
+        //using reloadData is the dumb way of doing it - it is not good for the user's perspective.
+        //[self.tableView reloadData];
+
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+
+        //if the user tap add icon - you need to actually update the data model and the tableview itself.
+    } else if (editingStyle == UITableViewCellEditingStyleInsert){
+        IconSet *set = self.iconSets[indexPath.section];
+        Icon *newIcon = [[Icon alloc] initWithTitle:@"New Icon" subtitle:@"" imageName:nil];
+        [set.icons addObject:newIcon];
+        [tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+
+    }
+}
+
 //step 2. conform the class to the delegate and data source protocols.
+//we need to know if we're in editing mode.
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+
+    //adjustment lets us know if we need an aditional row - if we're in editing mode. If we're not in editing mode, then it just returns the number of rows without the adjustment.
+
+    int adjustment = [self isEditing] ? 1 : 0 ;
 
     IconSet *set = self.iconSets[section];
 
-    return set.icons.count;
+    return set.icons.count + adjustment;
 
 }
+
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
 
     //create the cell based on prototype.
@@ -64,20 +101,64 @@
 
     IconSet *set = self.iconSets[indexPath.section];
 
-    Icon *icon = set.icons[indexPath.row];
+    //Configure the Cell
 
-    //fill in the cell details.
+    //here we need to see if the current number of rows are greater than the number of icons to display..and if it's editing mode then we want to display add icon to let the user know that he can tap to add another icon...else the tableview is not in editing mode which means we can just return the number of icons.
+    if(indexPath.row >= set.icons.count && [self isEditing]){
+        cell.textLabel.text = @"Add Icon";
+        cell.detailTextLabel.text = nil;
+        cell.imageView.image = nil;
+    }else{
 
-
-    cell.textLabel.text = icon.title;
-    cell.detailTextLabel.text = icon.subtitle;
-    cell.imageView.image = icon.image;
-
+          Icon *icon = set.icons[indexPath.row];
+        cell.textLabel.text = icon.title;
+        cell.detailTextLabel.text = icon.subtitle;
+        cell.imageView.image = icon.image;
+    }
 
 
 
     return cell;
 }
 
+//To let the tableView know that you went to edit mode and that it needs to update its self...you must overide setediting methethod...to tell the tableView everything you want to change.
 
+-(void)setEditing:(BOOL)editing animated:(BOOL)animated{
+    [super setEditing:editing animated:animated];
+
+    if (editing) {
+
+        //first we need to loop through all the sections.One insert for every section. This handles the case when we enter editing mode.
+        [self.tableView beginUpdates];
+        for (int i = 0; i < _iconSets.count; i++) {
+            IconSet *set = _iconSets[i];
+            //notify the tableview that we're trying to insert a new row.
+            [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:set.icons.count inSection:i]] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+        [self.tableView endUpdates];
+    }else {
+        //first we need to loop through all the sections.We delete one row at a time.  This handles the case when we enter editing mode.
+        [self.tableView beginUpdates];
+        for (int i = 0; i < _iconSets.count; i++) {
+            IconSet *set = _iconSets[i];
+            //notify the tableview that we're trying to insert a new row.
+            [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:set.icons.count inSection:i]] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+        [self.tableView endUpdates];
+    }
+}
+
+//To add the green button to the left of the cell for the user to tap. or red button for the user to delete, we must implement the following method.
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    IconSet *set = self.iconSets[indexPath.section];
+
+    if (indexPath.row >= set.icons.count) {
+
+        return UITableViewCellEditingStyleInsert;
+    } else {
+        return UITableViewCellEditingStyleDelete;
+    }
+}
 @end
