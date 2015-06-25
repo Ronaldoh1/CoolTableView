3241a06a5e83d38400729b70ae98ae96f03677b3
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-06-25 03:28:55 -0400

    able to move rows in table view using standar apple way and also added gesture recognizer to recognize long press and allow user to move rows

--- PrettyIcons2/ViewController.m
+++ PrettyIcons2/ViewController.m
@@ -38,6 +38,14 @@
     self.tableView.allowsSelectionDuringEditing =YES;
 
 
+
+     //Add the gesture recognizer
+    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPressGestureRecognized:)];
+
+    //add the long press recognizer to the table view.
+    [self.tableView addGestureRecognizer:longPress];
+
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,6 +54,143 @@
 }
 
 
+//****************Long Press Recognizer/////////
+//this is the method that will be called every time the user performs a long press.
+-(IBAction)longPressGestureRecognized:(UILongPressGestureRecognizer *)longPress{
+
+    CGPoint location = [longPress locationInView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
+
+    //get the current state of the gesture.
+
+    UIGestureRecognizerState state = longPress.state;
+    static UIView *snapShot = nil;
+    static NSIndexPath *sourceIndexPath = nil;
+
+    switch (state) {
+        case UIGestureRecognizerStateBegan: {
+            //check to see if we have an indexPath to make sure the user hasn't long pressed in an area that doesn't have an indexPath.
+
+            //if we do have an indexPath, then set the source indexpath to the indexpath that's recognized by the gesture recognizer above.
+
+            if (indexPath) {
+                sourceIndexPath = indexPath;
+
+                //get the cell to be moved.
+                UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+
+
+                snapShot = [self customSnapshotFromView:cell];
+
+                __block CGPoint center = cell.center;
+                snapShot.center = cell.center;
+                snapShot.alpha = 0;
+
+
+                [self.tableView addSubview:snapShot];
+                [UIView animateWithDuration:0.25 animations:^{
+
+                    center.y = location.y;
+                    snapShot.center = center;
+                    snapShot.transform = CGAffineTransformMakeScale(1.05, 1.05);
+                    snapShot.alpha = 0.98;
+
+
+                    cell.backgroundColor = [UIColor whiteColor];
+                    cell.textLabel.alpha = 0;
+                    cell.detailTextLabel.alpha = 0;
+                    cell.imageView.alpha = 0;
+
+
+                }];
+            }
+        }
+            break;
+
+        case UIGestureRecognizerStateChanged: {
+            CGPoint center = snapShot.center;
+            center.y = location.y;
+            snapShot.center = center;
+
+            IconSet *destSet = [self.iconSets objectAtIndex:indexPath.section];
+            if (indexPath && ![indexPath isEqual:sourceIndexPath] && indexPath.row < destSet.icons.count) {
+
+
+
+                IconSet *sourceSet = self.iconSets[sourceIndexPath.section];
+                IconSet *destSet = self.iconSets[indexPath.section];
+
+                Icon *iconToMove = sourceSet.icons[sourceIndexPath.row];
+
+                //need to check if the source set = destination set. if they are the same we are basically moving the icon to a different stop in the same array.
+                if (sourceSet == destSet) {
+
+                    //use the exchange object at index method to change places of each item within the same array.
+                    [destSet.icons exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndexPath.row];
+
+
+                } else{
+                    //else if the source set and destination sets are not the same, then we need to add the icon to the destination set and then remove it from the destination set.
+
+                    //1. We need to add our icon to our destination set.
+                    [destSet.icons insertObject:iconToMove atIndex:indexPath.row];
+                    
+                    //2. we need to remove it from our source set.
+                    [sourceSet.icons removeObjectAtIndex:sourceIndexPath.row];
+                    
+                    
+                    
+                    
+                }
+
+                //tell the table that you're moving the item from source indexPath to indexPath.
+                [self.tableView moveRowAtIndexPath:sourceIndexPath toIndexPath:indexPath];
+
+                sourceIndexPath = indexPath;
+                
+            }
+
+        }
+            break;
+
+        default:{
+
+            UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:sourceIndexPath];
+            [UIView animateWithDuration:0.25 animations:^{
+
+                snapShot.transform = CGAffineTransformMakeScale(1.0, 1.0);
+                snapShot.alpha = 0.00;
+
+
+                cell.backgroundColor = [UIColor whiteColor];
+                cell.textLabel.alpha = 1;
+                cell.detailTextLabel.alpha = 1;
+                cell.imageView.alpha = 1;
+            }completion:^(BOOL finished) {
+                [snapShot removeFromSuperview];
+                snapShot = nil;
+            }];
+        }
+            break;
+    }
+
+
+}
+
+//helper method to get a costum snap shot//
+-(UIView *)customSnapshotFromView:(UIView *)inputView{
+
+    UIView *snapshot = [inputView snapshotViewAfterScreenUpdates:YES];
+    snapshot.layer.masksToBounds = NO;
+    snapshot.layer.cornerRadius = 0.0;
+    snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0);
+    snapshot.layer.shadowRadius = 5.0;
+    snapshot.layer.shadowOpacity = 0.4;
+
+
+    return snapshot;
+
+}
 //*******************Moving Rows*************************
 -(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
 
@@ -89,7 +234,26 @@
 
 }
 
+//This method will check to seee if the indexPath to move the item if not return a different index path. Check if the proposed destination indexpath and to see if the user is trying to put it below the ADD button.
+-(NSIndexPath *)tableView:(UITableView *)tableView targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath{
 
+
+    //get the set for the seciton selected.
+    IconSet *set = self.iconSets[proposedDestinationIndexPath.section];
+
+    //check if the user is trying to place the cell beyond/below the add button. if the user tries, simply return the closes indexPath
+    if (proposedDestinationIndexPath.row >= set.icons.count) {
+
+        return [NSIndexPath indexPathForRow:set.icons.count -1 inSection:proposedDestinationIndexPath.section];
+
+
+    }
+
+    //else its ok let the user place the cell at the desired destination.
+    return proposedDestinationIndexPath;
+
+
+}
 //in willSelectRowAtIndexPath - this will return the indexPath only it's the last item in the section and it's in editing mode. It will not allow the user to select other cells. This is basically so that if the user taps on the last cell (to add a new icon) no matter where the user taps, the table view will know that the user is trying to add a new item
 
 -(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath{
